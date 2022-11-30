49369c63cf3892aa35a97e3cbf8d01e815a17e6f
mrkrausestem <33553266+mrkrausestem@users.noreply.github.com>
2022-11-30 08:12:25 -0500

    Initial Commit

--- ZPG---Mod/main.swift
+++ ZPG---Mod/main.swift
@@ -1,11 +1,32 @@
 //
 //  main.swift
-//  ZPG---Mod
+//  Zero Player Game
 //
-//  Created by Krause, Matthew on 11/30/22.
+//  Created by admin on 11/19/19.
+//  Copyright © 2019 admin. All rights reserved.
 //
 
 import Foundation
 
-print("Hello, World!")
+print("Welcome to the Dungeon!")
+
+// HERO SETUP
+// setup array of heroes
+var heroes : [Adventurer] = []
+var bluto = Warrior()
+bluto.name = "Bluto"
+heroes.append(bluto)
+
+var blammo = Mage()
+blammo.name = "Blammo"
+heroes.append(blammo)
+
+var blippo = Rogue()
+blippo.name = "Blippo"
+heroes.append(blippo)
+
+blippo.status()
+bluto.status()
+blammo.status()
+
 
